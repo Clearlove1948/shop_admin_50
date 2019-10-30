6b92251dc33d5e52c3c5a85eca5b59e148ca1ade
Clearlove1948 <740952732@qq.com>
2019-10-30 16:28:20 +0800

    登录功能完成

---
+++ src/components/Login.vue
@@ -0,0 +1,130 @@
+
+<template>
+  <div class="login">
+    <el-form :model="form" class="myForm" label-width="80px" :rules="rules" ref="aa">
+      <el-form-item label="用户名" prop="username">
+        <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input placeholder="请输入密码" v-model="form.password" show-password></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="success" class="logBtn" @click="fn2">登录</el-button>
+        <el-button type="danger" class="logReset" @click="fn">重置</el-button>
+      </el-form-item>
+      <img src="../imgs/QQ图片20190921235230.jpg" alt="" class="headImg">
+    </el-form>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+
+  data () {
+    return {
+      form: {
+        username: '',
+        password: '',
+        input: ''
+      },
+      rules: {
+        username: [
+          // required ---是否加必填项星号 message:判断是否输入用户名 trigger ---检验方式
+          // change 输入事件 blur 焦点事件
+          { required: true, message: '请输入用户名', trigger: ['change', 'blur'] },
+          { min: 3, max: 12, message: '用户名必须是3-12位', trigger: ['change', 'blur'] }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: ['change', 'blur'] },
+          { min: 3, max: 12, message: '用户名必须是3-12位', trigger: ['change', 'blur'] }
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    },
+    fn () {
+      this.$refs.aa.resetFields()
+    },
+    fn2 () {
+      this.$refs.aa.validate((flag) => {
+        if (!flag) return
+        console.log('发送ajax')
+        // eslint-disable-next-line no-undef
+        // axios({
+        //   data: this.form,
+        //   url: 'http://localhost:8888/api/private/v1/login',
+        //   method: 'post'
+        // }).then((res) => {
+        //   // eslint-disable-next-line no-unused-vars
+        //   const { meta } = res.data
+        //   if (meta.status === 200) {
+        //     console.log('登录成功')
+        //   } else {
+        //     console.log(meta.msg)
+        //   }
+        // })
+        // 优化后
+        axios.post('http://localhost:8888/api/private/v1/login', this.form).then((res) => {
+          // eslint-disable-next-line no-unused-vars
+          const { meta } = res.data
+          if (meta.status === 200) {
+            this.$message({
+              // 设置提示语 颜色 持续时间 (提示框的设置)
+              message: meta.msg,
+              type: 'success',
+              duration: 1000
+            })
+            // 跳转到首页
+            this.$router.push({ name: '/index' })
+          } else {
+            console.log(meta.msg)
+            this.$message.error({
+              message: meta.msg,
+              duration: 1000
+            })
+          }
+        })
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .login{
+    width:100%;
+    height:100%;
+    background-color: #2d434c;
+    overflow: hidden;
+    .el-form{
+    width:400px;
+    // height: 200px;
+    background-color: #fff;
+    border-radius: 20px;
+    padding: 20px;
+    padding-top: 70px;
+    margin: 0 auto;
+    margin-top: 200px ;
+    position: relative;
+    .headImg{
+      width:120px;
+      height:120px;
+      position: absolute;
+      top:-25%;
+      left:50%;
+      margin-left: -60px;
+      border-radius: 50%;
+      border:5px solid #fff;
+    }
+    .logReset{
+      margin-left: 100px;
+    }
+  }
+  }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Index from '../components/index.vue'
 
 Vue.use(VueRouter)
 
@@ -8,15 +9,17 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    redirect: '/Index'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/index',
+    name: 'index',
+    component: Index
   }
 ]
 
